10f0e25acc11bf6134e4db10486a5564a395a0a6
Syadon786 <bokagabor4@gmail.com>
2020-10-06 21:52:14 +0200

    harmadik

--- src/Content.ts
+++ src/Content.ts
@@ -22,7 +22,7 @@ export default class Content {
         res.write("<head>");
         res.write("<style>input, pre {font-family:monospace; font-size:1em; font-weight:bold;}</style>");
         res.write("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
-        res.write("<title>Jedlik Ts Template</title>");
+        res.write("<title>Euklideszi algoritmus</title>");
         res.write("</head>");
         res.write("<body><form><pre>");
 
@@ -33,11 +33,11 @@ export default class Content {
         const x = url.parse(req.url as string, true).query;
         const y = url.parse(req.url as string, true).query;
         let elso: number = parseInt(x.elso as string);
-        let masodik: number = parseInt(x.elso as string);
-        if (isNaN(elso) || elso < 0) elso = 0; // egy kis ellenőrzés
-        res.write(`Kérem az első számot: <input type='text' name='elso' value=${elso} style='width:3em;' onChange='this.form.submit();'>\n`);
-        if (isNaN(masodik) || masodik < 0) masodik = 0; // egy kis ellenőrzés
-        res.write(`Kérem a második számot: <input type='text' name='masodik' value=${masodik} style='width:3em;' onChange='this.form.submit();'>\n`);
+        let masodik: number = parseInt(y.masodik as string);
+        if (isNaN(elso) || elso < 0 || elso > 999999999999999) elso = 0; // egy kis ellenőrzés
+        res.write(`Kérem az első számot [0-999999999999999]: <input type='text' name='elso' value=${elso} style='width:3em;' onChange='this.form.submit();'>\n`);
+        if (isNaN(masodik) || masodik < 0 || masodik > 999999999999999) masodik = 0; // egy kis ellenőrzés
+        res.write(`Kérem a második számot [0-999999999999999]: <input type='text' name='masodik' value=${masodik} style='width:3em;' onChange='this.form.submit();'>\n`);
         res.write(`Az első szám ${elso}!\n`);
         res.write(`Az első szám ${masodik}!\n`);
 
