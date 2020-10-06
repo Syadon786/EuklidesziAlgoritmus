70cd7fe8b84d552e877e79c78b4a1d0ae7c5002d
Syadon786 <bokagabor4@gmail.com>
2020-10-06 21:45:34 +0200

    masodik

--- src/Content.ts
+++ src/Content.ts
@@ -28,32 +28,18 @@ export default class Content {
 
         // Kezd a kódolást innen -->
 
-        res.write("Egyszerű Hello World!\n");
-
-        // Tetszőleges html teg-ek és attribútumok beépítése:
-        res.write("<span style='color: blue;'><i>Színes és dőlt Hello World!'</i></span>\n");
-
-        // Egyszerű input JSON állományból:
-        const input: InputInterface = JSON.parse(fs.readFileSync("input.json", "utf-8"));
-
-        // String template használata
-        res.write(`1. feladat: ${input.name} kora: ${input.age}\n`);
-
-        // Változó definiálása:
-        let neme = "";
-        neme = input.male ? "férfi" : "nő";
-        res.write(`2. feladat: ${input.name} neme: ${neme}\n`);
-
         // Input form-al és <input type='number'>-el:
         // URL paraméter(ek) (itt "kor") ellenőrzése,  kiolvasása, alapértelmezett értéke 18:
-        const u = url.parse(req.url as string, true).query;
-        // ha "kor" paraméter nincs megadva vagy "kor" paraméter üres string,
-        // akkor legyen 18 az értéke, egyébként konvertáljuk számra a "kor" paraméter értékét:
-        // let kor: number = u.kor === undefined || u.kor === "" ? 18 : parseInt(u.kor as string);
-        let kor: number = parseInt(u.kor as string);
-        if (isNaN(kor) || kor < 0 || kor > 99) kor = 18; // egy kis ellenőrzés
-        res.write(`3. feladat: Kérem a korod [0-99]: <input type='text' name='kor' value=${kor} style='width:3em;' onChange='this.form.submit();'>\n`);
-        res.write(`4. feladat: Te ${kor} éves vagy!\n`);
+        const x = url.parse(req.url as string, true).query;
+        const y = url.parse(req.url as string, true).query;
+        let elso: number = parseInt(x.elso as string);
+        let masodik: number = parseInt(x.elso as string);
+        if (isNaN(elso) || elso < 0) elso = 0; // egy kis ellenőrzés
+        res.write(`Kérem az első számot: <input type='text' name='elso' value=${elso} style='width:3em;' onChange='this.form.submit();'>\n`);
+        if (isNaN(masodik) || masodik < 0) masodik = 0; // egy kis ellenőrzés
+        res.write(`Kérem a második számot: <input type='text' name='masodik' value=${masodik} style='width:3em;' onChange='this.form.submit();'>\n`);
+        res.write(`Az első szám ${elso}!\n`);
+        res.write(`Az első szám ${masodik}!\n`);
 
         // <---- Fejezd be a kódolást
 
